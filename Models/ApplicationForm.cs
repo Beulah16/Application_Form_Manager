72b432d7313233599dfeae9be2ef5676be9faeb7
Beulah16 <igboanugobeulah16@gmail.com>
2024-06-19 06:22:47 +0100

    Created Models and Helpers

---
+++ Models/ApplicationForm.cs
@@ -0,0 +1,14 @@
+using ApplicationFormManager.Helpers;
+
+namespace ApplicationFormManager.Models
+{
+    public class ApplicationForm
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public List<InputField> PersonalInputFields { get; set; } = [];
+        public List<CustomQuestion> CustomQuestionFields { get; set; } = [];
+
+    }
+}
